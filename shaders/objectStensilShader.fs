4caf1e610aa213cbf118d26b10997e018b3b87a3
RGB20 <rudraksha.shah@gmail.com>
2023-03-10 17:19:49 -0500

    Stensil shading test, plus some addition to depth shaders

--- shaders/objectStensilShader.fs
+++ shaders/objectStensilShader.fs
@@ -96,7 +96,7 @@ void main()
     result += CalcSpotLight(spotLight, norm, FragPos, viewDir);
 
     float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far for demonstration
-    FragColor = vec4(vec3(depth), 1.0);
+    FragColor = vec4(0.04, 0.28, 0.26, 1.0);
 }
 
 // Lighting Functions Definations
--- src/Main.cpp
+++ src/Main.cpp
@@ -124,8 +124,13 @@ int main()
     }
 
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    // configure global opengl state
+    // -----------------------------
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
+    glEnable(GL_STENCIL_TEST);
+    glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
@@ -140,17 +145,25 @@ int main()
     // -------------------------------------------------------------------------------------------------- //
     
     // Object shader program and other handlers
-    std::string objectVertexShaderPath = GetCurrentDir() + "\\shaders\\objectDepthTesting.vs";
-    std::string objectFragmentShaderPath = GetCurrentDir() + "\\shaders\\objectDepthTesting.fs";
+    std::string objectStensilVertexShaderPath = GetCurrentDir() + "\\shaders\\objectStensilShader.vs";
+    std::string objectStensilFragmentShaderPath = GetCurrentDir() + "\\shaders\\objectStensilShader.fs";
+    Shader objectStensilShaderProgram(objectStensilVertexShaderPath.c_str(), objectStensilFragmentShaderPath.c_str());
+
+    std::string objectVertexShaderPath = GetCurrentDir() + "\\shaders\\objectVertexShader.vs";
+    std::string objectFragmentShaderPath = GetCurrentDir() + "\\shaders\\objectFragmentShader.fs";
     Shader objectShaderProgram(objectVertexShaderPath.c_str(), objectFragmentShaderPath.c_str());
     
     std::string containerDiffuseTexMap = GetCurrentDir() + "\\textures\\";
     unsigned int texture1 = TextureFromFile("containerDiffuseMap.png", containerDiffuseTexMap.c_str());
+    std::string containerSpecularTexMap = GetCurrentDir() + "\\textures\\";
+    unsigned int texture1_2 = TextureFromFile("containerSpecularMap.png", containerSpecularTexMap.c_str());
 
     std::string wallDiffuseTexMap = GetCurrentDir() + "\\textures\\";
     unsigned int texture2 = TextureFromFile("wall.jpg", containerDiffuseTexMap.c_str());
     
     objectShaderProgram.setInt("material.diffuse", 0);
+    objectShaderProgram.setInt("material.specular", 1);
+    objectStensilShaderProgram.setInt("material.diffuse", 0);
 
 
     // cube VAO
@@ -310,7 +323,7 @@ int main()
         // clear render targets
         // ------
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
         // RENDER OBJECTS
         objectShaderProgram.use();
@@ -328,14 +341,6 @@ int main()
         // FS stage Uniform inputs
         objectShaderProgram.setVec3("viewPos", camera.Position);
 
-        // Set the material diffuse and specular maps
-        // texture1 - Material diffuse
-        // texture2 - Material specular 
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        //glActiveTexture(GL_TEXTURE1);
-        //glBindTexture(GL_TEXTURE_2D, texture2);
-        objectShaderProgram.setVec3("material.specular", glm::vec3(0.5,0.5,0.5));
         objectShaderProgram.setFloat("material.shininess", 32.0f);
 
         //DIRECTIONAL LIGHT
@@ -376,10 +381,39 @@ int main()
             objectShaderProgram.setVec3("pointLights[" + number_str + "].specular", glm::vec3(1.0f, 1.0f, 1.0f));
         }
 
+        // draw floor as normal, but don't write the floor to the stencil buffer, we only care about the containers. We set its mask to 0x00 to not write to the stencil buffer.
+        glStencilMask(0x00); // make sure we don't update the stencil buffer while drawing the floor
+
+        // Planes VAO
+        glBindVertexArray(planeVAO);
+        // Set the material diffuse and specular maps
+        // texture1 - Material diffuse
+        // texture2 - Material specular 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture1_2);
+
+        // Plane
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[1]); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        objectShaderProgram.setMat4("model", model);
+        objectShaderProgram.setMat3("modelInvT", glm::mat3(glm::transpose(glm::inverse(model))));
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // 1st. render pass, draw objects as normal, writing to the stencil buffer
+        // --------------------------------------------------------------------
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
+        glStencilMask(0xFF);
+
         // Cubes VAO
         glBindVertexArray(cubeVAO);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture1_2);
         {
             // Cube 1
             glm::mat4 model = glm::mat4(1.0f);
@@ -399,18 +433,102 @@ int main()
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
-        // Planes VAO
-        glBindVertexArray(planeVAO);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture2);
+        // 2nd. render pass: now draw slightly scaled versions of the objects, this time disabling stencil writing.
+        // Because the stencil buffer is now filled with several 1s. The parts of the buffer that are 1 are not drawn, thus only drawing 
+        // the objects' size differences, making it look like borders.
+        // -----------------------------------------------------------------------------------------------------------------------------
+        glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+        glStencilMask(0x00);
+        glDisable(GL_DEPTH_TEST);
+        objectStensilShaderProgram.use();
+        {
+            // VS stage Uniform inputs
+            // Uniforms are bound to the shader program and do not care if you access them from the VS of FS stage
+            // Saying VS stage Uniform inputs is just a comment to make code easy to read and debug
+            // You can set the uniforms once or every frame
+            // View
+            objectStensilShaderProgram.setMat4("view", camera.GetViewMatrix());
+            // Projection
+            projection = glm::perspective(glm::radians(ZOOM), float(SCR_WIDTH) / float(SCR_HEIGHT), 0.1f, 100.0f);
+            objectStensilShaderProgram.setMat4("projection", projection);
 
-        // Plane
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, cubePositions[1]); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
-        objectShaderProgram.setMat4("model", model);
-        objectShaderProgram.setMat3("modelInvT", glm::mat3(glm::transpose(glm::inverse(model))));
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            // FS stage Uniform inputs
+            objectStensilShaderProgram.setVec3("viewPos", camera.Position);
+
+            // Set the material diffuse and specular maps
+            // texture1 - Material diffuse
+            // texture2 - Material specular 
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, texture1);
+            //glActiveTexture(GL_TEXTURE1);
+            //glBindTexture(GL_TEXTURE_2D, texture2);
+            objectStensilShaderProgram.setVec3("material.specular", glm::vec3(0.5, 0.5, 0.5));
+            objectStensilShaderProgram.setFloat("material.shininess", 32.0f);
+
+            //DIRECTIONAL LIGHT
+            glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
+            glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
+            objectStensilShaderProgram.setVec3("dirLight.direction", directionalLightDirection);
+            objectStensilShaderProgram.setVec3("dirLight.ambient", ambientColor);
+            objectStensilShaderProgram.setVec3("dirLight.diffuse", diffuseColor); // darken diffuse light a bit
+            objectStensilShaderProgram.setVec3("dirLight.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+
+            // SPOT LIGHT
+            objectStensilShaderProgram.setVec3("spotLight.position", camera.Position);
+            objectStensilShaderProgram.setVec3("spotLight.direction", camera.Front);
+            objectStensilShaderProgram.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+            objectStensilShaderProgram.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(17.5f)));
+
+            objectStensilShaderProgram.setVec3("spotLight.ambient", ambientColor);
+            objectStensilShaderProgram.setVec3("spotLight.diffuse", diffuseColor); // darken diffuse light a bit
+            objectStensilShaderProgram.setVec3("spotLight.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+
+            objectStensilShaderProgram.setFloat("spotLight.constant", 1.0f);
+            objectStensilShaderProgram.setFloat("spotLight.linear", 0.09f);
+            objectStensilShaderProgram.setFloat("spotLight.quadratic", 0.032f);
+
+            // POINT LIGHT
+            // This should be the same as the number of points lights we statically define in the shader
+            // TODO: Make this dynamic
+            int pointLights = 4;
+            for (int i = 0; i < pointLights; ++i)
+            {
+                std::string number_str = std::to_string(i);
+                objectStensilShaderProgram.setVec3("pointLights[" + number_str + "].position", pointLightPositions[i]);
+                objectStensilShaderProgram.setFloat("pointLights[" + number_str + "].constant", 1.0f);
+                objectStensilShaderProgram.setFloat("pointLights[" + number_str + "].linear", 0.09f);
+                objectStensilShaderProgram.setFloat("pointLights[" + number_str + "].quadratic", 0.032f);
+                objectStensilShaderProgram.setVec3("pointLights[" + number_str + "].ambient", ambientColor);
+                objectStensilShaderProgram.setVec3("pointLights[" + number_str + "].diffuse", diffuseColor); // darken diffuse light a bit
+                objectStensilShaderProgram.setVec3("pointLights[" + number_str + "].specular", glm::vec3(1.0f, 1.0f, 1.0f));
+            }
+
+            // Cubes VAO
+            glBindVertexArray(cubeVAO);
+            {
+                // Cube 1
+                glm::mat4 model = glm::mat4(1.0f);
+                model = glm::translate(model, cubePositions[0]); // translate it down so it's at the center of the scene
+                model = glm::scale(model, glm::vec3(1.2f, 1.2f, 1.2f));	// it's a bit too big for our scene, so scale it down
+                objectStensilShaderProgram.setMat4("model", model);
+                objectStensilShaderProgram.setMat3("modelInvT", glm::mat3(glm::transpose(glm::inverse(model))));
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
+            {
+                // Cube 2
+                glm::mat4 model = glm::mat4(1.0f);
+                model = glm::translate(model, cubePositions[1]); // translate it down so it's at the center of the scene
+                model = glm::scale(model, glm::vec3(1.2f, 1.2f, 1.2f));	// it's a bit too big for our scene, so scale it down
+                objectStensilShaderProgram.setMat4("model", model);
+                objectStensilShaderProgram.setMat3("modelInvT", glm::mat3(glm::transpose(glm::inverse(model))));
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
+            glBindVertexArray(0);
+        }
+        glStencilMask(0xFF);
+        glStencilFunc(GL_ALWAYS, 0, 0xFF);
+        glEnable(GL_DEPTH_TEST);
+
         //backpackModel.Draw(objectShaderProgram);
 
         
