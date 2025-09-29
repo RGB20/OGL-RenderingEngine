347080e35179f983a06d14129a30d4ff4026b440
RGB20 <rudraksha.shah@gmail.com>
2025-09-28 23:06:24 -0400

    Added Parallex Occlusion Mapping, Steep Parallax Mapping

--- shaders/parallaxMappingFragmentShader.fs
+++ shaders/parallaxMappingFragmentShader.fs
@@ -22,9 +22,51 @@ uniform float height_scale;
 
 vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
 {
-    float height =  texture(material.depthMap, texCoords).r;    
-    vec2 p = viewDir.xy / viewDir.z * (height * height_scale);
-    return clamp(texCoords - p, 0.0f, 1.0f);    
+    // Steep Paralammax Mapping
+    // number of depth layers
+    const float minLayers = 8.0;
+    const float maxLayers = 32.0;
+    float numLayers = mix(maxLayers, minLayers, max(dot(vec3(0.0, 0.0, 1.0), viewDir), 0.0));
+    // calculate the size of each layer
+    float layerDepth = 1.0 / numLayers;
+    // depth of current layer
+    float currentLayerDepth = 0.0;
+    // the amount to shift the texture coordinates per layer (from vector P)
+    vec2 P = viewDir.xy * height_scale; 
+    vec2 deltaTexCoords = P / numLayers;
+
+    // get initial values
+    vec2  currentTexCoords     = texCoords;
+    float currentDepthMapValue = texture(material.depthMap, currentTexCoords).r;
+
+    while(currentLayerDepth < currentDepthMapValue)
+    {
+        // shift texture coordinates along direction of P
+        currentTexCoords -= deltaTexCoords;
+        // get depthmap value at current texture coordinates
+        currentDepthMapValue = texture(material.depthMap, currentTexCoords).r;  
+        // get depth of next layer
+        currentLayerDepth += layerDepth;  
+    }
+
+    // get texture coordinates before collision (reverse operations)
+    vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
+
+    // get depth after and before collision for linear interpolation
+    float afterDepth  = currentDepthMapValue - currentLayerDepth;
+    float beforeDepth = texture(material.depthMap, prevTexCoords).r - currentLayerDepth + layerDepth;
+ 
+    // interpolation of texture coordinates
+    float weight = afterDepth / (afterDepth - beforeDepth);
+    vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
+
+    return finalTexCoords;  
+
+    //return currentTexCoords;
+
+//    float height =  texture(material.depthMap, texCoords).r;    
+//    vec2 p = viewDir.xy / viewDir.z * (height * height_scale);
+//    return texCoords - p;    
 }
 
 void main()
@@ -32,6 +74,8 @@ void main()
     // offset texture coordinates with Parallax Mapping
     vec3 viewDir   = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
     vec2 texCoords = ParallaxMapping(fs_in.TexCoords,  viewDir);
+    if(texCoords.x > 1.0 || texCoords.y > 1.0 || texCoords.x < 0.0 || texCoords.y < 0.0)
+        discard;
 
      // obtain normal from normal map in range [0,1]
     vec3 normal = texture(material.normalMap, texCoords).rgb;
